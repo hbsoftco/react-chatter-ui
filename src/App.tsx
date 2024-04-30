5363c60c267191aa19dfcd1b70660a399e3f8a95
Hossein <hossein.bajan@gmail.com>
2024-04-30 18:50:17 +0330

    Add login, signup and router file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-const App = ()=> {
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+const App = () => {
   return (
-    <div>hossein</div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
