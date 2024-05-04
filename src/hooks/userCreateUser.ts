e37c93931cfbdca45fc3214b382292a16117d6ae
Hossein <hossein.bajan@gmail.com>
2024-05-04 07:33:57 +0330

    Add User interface and add useCreateUser hook

---
+++ src/hooks/userCreateUser.ts
@@ -0,0 +1,24 @@
+import { gql, useMutation } from "@apollo/client";
+import { User } from "../models/User";
+
+interface CreateUserInput {
+  createUserData: {
+    email: string;
+    password: string;
+  };
+}
+
+const CREATE_USER = gql`
+  mutation CreateUser($createUserData: CreateUserInput!) {
+    createUser(createUserData: $createUserData) {
+      _id
+      email
+    }
+  }
+`;
+
+const useCreateUser = () => {
+  return useMutation<User, CreateUserInput>(CREATE_USER);
+};
+
+export { useCreateUser };
---
+++ src/models/AbstractModel.ts
@@ -0,0 +1,3 @@
+export interface AbstractModel {
+  email: string;
+}
