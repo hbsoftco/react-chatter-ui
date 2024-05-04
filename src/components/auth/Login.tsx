68772ef9d825065a75a61da4c12f5595e053dcea
Hossein <hossein.bajan@gmail.com>
2024-05-04 15:59:55 +0330

    Add useLogin hook and use it for login component

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel="Login" onSubmit={async () => {}}>
+    <Auth submitLabel="Login" onSubmit={async (request) => login(request)}>
       <Link to={"/signup"} style={{ alignSelf: "center" }}>
         <MUILink>Signup</MUILink>
       </Link>
