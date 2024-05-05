c305e9d2d286893ed4fcbea0df30412555b10bfa
Hossein <hossein.bajan@gmail.com>
2024-05-05 09:43:33 +0330

    Error handling in auth

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -2,21 +2,28 @@ import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
 import { useCreateUser } from "../../hooks/useCreateUser";
+import { useState } from "react";
 
 const Signup = () => {
   const [createUser] = useCreateUser();
+  const [error, setError] = useState();
   return (
     <Auth
       submitLabel="Register"
       onSubmit={async ({ email, password }) => {
-        await createUser({
-          variables: {
-            createUserInput: {
-              email,
-              password,
+        try {
+          await createUser({
+            variables: {
+              createUserInput: {
+                email,
+                password,
+              },
             },
-          },
-        });
+          });
+        } catch (error) {
+          console.log(error);
+          throw error;
+        }
       }}
     >
       <Link to={"/login"} style={{ alignSelf: "center" }}>
